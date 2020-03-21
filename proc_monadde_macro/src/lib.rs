ed4c6d1c01e5cd2fe2a979e3c913cd485fbed4f0
madwareru <madware.ru@gmail.com>
2020-03-21 14:10:45 +0300

    filtering feature

--- proc_monadde_macro/src/lib.rs
+++ proc_monadde_macro/src/lib.rs
@@ -15,10 +15,23 @@ impl AppendLine for String {
 pub fn define_monadde_macro(_: TokenStream) -> TokenStream {
     const DEPTH:i32 = 10; // make it bigger in the case you need more depth (thx Cap)
     let mut result_string = String::from("macro_rules! monadde {\n");
+
+    //map step (simplest variant possible)
     result_string.append_line("($e_in:expr => $i:ident |> $e_out:expr)");
     result_string.append_line("=> { $e_in.map(move |$i| $e_out) };");
+
+    //"filter_map" variant which takes care of filtering feature
+    result_string.append_line("($e_in:expr => $i:ident |> when $cond:expr => $e_out:expr)");
+    result_string.append_line("=> { $e_in.filter_map(move |$i| if $cond { Some($e_out) } else { None } ) };");
+
+    //root bind step
     result_string.append_line("($e_in0:expr => $i0:ident |> $e_in:expr => $i:ident |> $e_out:expr)");
     result_string.append_line("=> { $e_in0.flat_map(move |$i0| monadde!{ $e_in => $i |> $e_out }) };");
+
+    //root filtermappy bind
+    result_string.append_line("($e_in0:expr => $i0:ident |> $e_in:expr => $i:ident |> when $cond:expr => $e_out:expr)");
+    result_string.append_line("=> { $e_in0.flat_map(move |$i0| monadde!{ $e_in => $i |> when $cond => $e_out }) };");
+
     for i in 1..DEPTH {
         result_string.append_line("(");
         result_string.append_line("$e_in0:expr => $i0:ident |> ");
@@ -34,6 +47,25 @@ pub fn define_monadde_macro(_: TokenStream) -> TokenStream {
             result_string.append_line(&formatted);
         }
         result_string.append_line("$e_in => $i |> $e_out");
+        result_string.append_line("})");
+        result_string.append_line("};")
+    }
+
+    for i in 1..DEPTH {
+        result_string.append_line("(");
+        result_string.append_line("$e_in0:expr => $i0:ident |> ");
+        for j in 1..=i {
+            let formatted = format!("$e_in{}:expr => $i{}:ident |> ", j, j);
+            result_string.append_line(&formatted);
+        }
+        result_string.append_line("$e_in:expr => $i:ident |> when $cond:expr => $e_out:expr");
+        result_string.append_line(") => {");
+        result_string.append_line("$e_in0.flat_map(move |$i0| monadde!{");
+        for j in 1..=i {
+            let formatted = format!("$e_in{} => $i{} |> ", j, j);
+            result_string.append_line(&formatted);
+        }
+        result_string.append_line("$e_in => $i |> when $cond => $e_out");
         result_string.append_line("})");
         result_string.append_line("};")
     }
