e8b3bb32a47ec60b4b6a8db29150fdc4bf4c1178
Vitaly <madware.ru@gmail.com>
2020-03-21 11:02:51 +0300

    fix to match standart monadic primitives of rust

--- proc_monadde_macro/src/lib.rs
+++ proc_monadde_macro/src/lib.rs
@@ -16,9 +16,9 @@ pub fn define_monadde_macro(_: TokenStream) -> TokenStream {
     const DEPTH:i32 = 10; // make it bigger in the case you need more depth (thx Cap)
     let mut result_string = String::from("macro_rules! monadde {\n");
     result_string.append_line("($e_in:expr => $i:ident |> $e_out:expr)");
-    result_string.append_line("=> { $e_in.fmap(|$i| $e_out) };");
+    result_string.append_line("=> { $e_in.map(|$i| $e_out) };");
     result_string.append_line("($e_in0:expr => $i0:ident |> $e_in:expr => $i:ident |> $e_out:expr)");
-    result_string.append_line("=> { $e_in0.bind(|$i0| monadde!{ $e_in => $i |> $e_out }) };");
+    result_string.append_line("=> { $e_in0.flat_map(|$i0| monadde!{ $e_in => $i |> $e_out }) };");
     for i in 1..DEPTH {
         result_string.append_line("(");
         result_string.append_line("$e_in0:expr => $i0:ident |> ");
@@ -28,7 +28,7 @@ pub fn define_monadde_macro(_: TokenStream) -> TokenStream {
         }
         result_string.append_line("$e_in:expr => $i:ident |> $e_out:expr");
         result_string.append_line(") => {");
-        result_string.append_line("$e_in0.bind(|$i0| monadde!{");
+        result_string.append_line("$e_in0.flat_map(|$i0| monadde!{");
         for j in 1..=i {
             let formatted = format!("$e_in{} => $i{} |> ", j, j);
             result_string.append_line(&formatted);
