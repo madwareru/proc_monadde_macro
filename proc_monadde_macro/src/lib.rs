35a2c8da0d63bbb45425c733fdba13237f79f725
madwareru <madware.ru@gmail.com>
2020-03-21 10:54:11 +0300

    code style fix

--- proc_monadde_macro/src/lib.rs
+++ proc_monadde_macro/src/lib.rs
@@ -14,29 +14,29 @@ impl AppentLine for String {
 #[proc_macro]
 pub fn define_monadde_macro(_: TokenStream) -> TokenStream {
     const DEPTH:i32 = 10; // make it bigger in the case you need more depth (thx Cap)
-    let mut resultString = String::from("macro_rules! monadde {\n");
-    resultString.append_line("($e_in:expr => $i:ident |> $e_out:expr)");
-    resultString.append_line("=> { $e_in.fmap(|$i| $e_out) };");
-    resultString.append_line("($e_in0:expr => $i0:ident |> $e_in:expr => $i:ident |> $e_out:expr)");
-    resultString.append_line("=> { $e_in0.bind(|$i0| monadde!{ $e_in => $i |> $e_out }) };");
+    let mut result_string = String::from("macro_rules! monadde {\n");
+    result_string.append_line("($e_in:expr => $i:ident |> $e_out:expr)");
+    result_string.append_line("=> { $e_in.fmap(|$i| $e_out) };");
+    result_string.append_line("($e_in0:expr => $i0:ident |> $e_in:expr => $i:ident |> $e_out:expr)");
+    result_string.append_line("=> { $e_in0.bind(|$i0| monadde!{ $e_in => $i |> $e_out }) };");
     for i in 1..DEPTH {
-        resultString.append_line("(");
-        resultString.append_line("$e_in0:expr => $i0:ident |> ");
+        result_string.append_line("(");
+        result_string.append_line("$e_in0:expr => $i0:ident |> ");
         for j in 1..=i {
             let formatted = format!("$e_in{}:expr => $i{}:ident |> ", j, j);
-            resultString.append_line(&formatted);
+            result_string.append_line(&formatted);
         }
-        resultString.append_line("$e_in:expr => $i:ident |> $e_out:expr");
-        resultString.append_line(") => {");
-        resultString.append_line("$e_in0.bind(|$i0| monadde!{");
+        result_string.append_line("$e_in:expr => $i:ident |> $e_out:expr");
+        result_string.append_line(") => {");
+        result_string.append_line("$e_in0.bind(|$i0| monadde!{");
         for j in 1..=i {
             let formatted = format!("$e_in{} => $i{} |> ", j, j);
-            resultString.append_line(&formatted);
+            result_string.append_line(&formatted);
         }
-        resultString.append_line("$e_in => $i |> $e_out");
-        resultString.append_line("})");
-        resultString.append_line("};")
+        result_string.append_line("$e_in => $i |> $e_out");
+        result_string.append_line("})");
+        result_string.append_line("};")
     }
-    resultString.append_line("}");
-    (&resultString).parse::<TokenStream>().unwrap()
+    result_string.append_line("}");
+    (&result_string).parse::<TokenStream>().unwrap()
 }
